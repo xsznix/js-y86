535c84ab7961b55db6a5068e4de61095ff8d8375
Xuming Zeng <xsznix@gmail.com>
2015-03-23 15:09:13 -0500

    pad 4 byte arguments when assembling

--- js/assem.js
+++ js/assem.js
@@ -1,53 +1,57 @@
 var ASSEM = [];
 
-ASSEM[0] = function () {
-	return '00';
-};
+(function () {
+	function padq (n) { if (n.length === 8) return n; else return toBigEndian(padHex(n, 8)); }
 
-ASSEM[1] = function () {
-	return '10';
-};
+	ASSEM[0] = function () {
+		return '00';
+	};
 
-ASSEM[2] = function () {
-	return '2' + this.fn + this.rA + this.rB;
-};
+	ASSEM[1] = function () {
+		return '10';
+	};
 
-ASSEM[3] = function () {
-	return '30f' + this.rB + this.V;
-};
+	ASSEM[2] = function () {
+		return '2' + this.fn + this.rA + this.rB;
+	};
 
-ASSEM[4] = function () {
-	return '40' + this.rA + this.rB + this.D;
-};
+	ASSEM[3] = function () {
+		return '30f' + this.rB + padq(this.V);
+	};
 
-ASSEM[5] = function () {
-	return '50' + this.rA + this.rB + this.D;
-};
+	ASSEM[4] = function () {
+		return '40' + this.rA + this.rB + padq(this.D);
+	};
 
-ASSEM[6] = function () {
-	return '6' + this.fn + this.rA + this.rB;
-};
+	ASSEM[5] = function () {
+		return '50' + this.rA + this.rB + padq(this.D);
+	};
 
-ASSEM[7] = function () {
-	return '7' + this.fn + this.Dest;
-};
+	ASSEM[6] = function () {
+		return '6' + this.fn + this.rA + this.rB;
+	};
 
-ASSEM[8] = function () {
-	return '80' + this.Dest;
-};
+	ASSEM[7] = function () {
+		return '7' + this.fn + padq(this.Dest);
+	};
 
-ASSEM[9] = function () {
-	return '90';
-};
+	ASSEM[8] = function () {
+		return '80' + padq(this.Dest);
+	};
 
-ASSEM[10] = function () {
-	return 'a0' + this.rA + 'f';
-};
+	ASSEM[9] = function () {
+		return '90';
+	};
 
-ASSEM[11] = function () {
-	return 'b0' + this.rA + 'f';
-};
+	ASSEM[10] = function () {
+		return 'a0' + this.rA + 'f';
+	};
 
-ASSEM[15] = function () {
-	return 'f' + this.fn;
-}
+	ASSEM[11] = function () {
+		return 'b0' + this.rA + 'f';
+	};
+
+	ASSEM[15] = function () {
+		return 'f' + this.fn;
+	}
+})();
