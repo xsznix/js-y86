xsznix/js-y86

commit 09c768541adaaa336882b87a4ca0da1857f2a73b

implement more runtime errors and show runtime error descriptions in status panel

js/instr.js
@@ -11,42 +11,42 @@ INSTR[2] = function () {
 	switch(this.fn) {
 		case 0:
 			// RRMOVL
-			REG[this.rB] = REG[this.rA];
+			REG[this.rB] = getRegister(this.rA);
 			break;
 		case 1:
 			// CMOVLE
 			if (SF === 1 || ZF === 1) {
-				REG[this.rB] = REG[this.rA];
+				REG[this.rB] = getRegister(this.rA);
 			}
 			break;
 		case 2:
 			// CMOVL
 			if (SF === 1) {
-				REG[this.rB] = REG[this.rA];
+				REG[this.rB] = getRegister(this.rA);
 			}
 			break;
 		case 3:
 			// CMOVE
 			if (ZF === 1) {
-				REG[this.rB] = REG[this.rA];
+				REG[this.rB] = getRegister(this.rA);
 			}
 			break;
 		case 4:
 			// CMOVNE
 			if (ZF === 0) {
-				REG[this.rB] = REG[this.rA];
+				REG[this.rB] = getRegister(this.rA);
 			}
 			break;
 		case 5:
 			// CMOVGE
 			if (SF === 0 || ZF === 1) {
-				REG[this.rB] = REG[this.rA];
+				getRegister(this.rB) = getRegister(this.rA);
 			}
 			break;
 		case 6:
 			// CMOVG
 			if (SF === 0 && ZF === 0) {
-				REG[this.rB] = REG[this.rA];
+				REG[this.rB] = getRegister(this.rA);
 			}
 			break;
 	}
@@ -55,46 +55,46 @@ INSTR[3] = function () {
 	REG[this.rB] = this.V;
 };
 INSTR[4] = function () {
-	var valA = REG[this.rA],
-		valB = REG[this.rB],
+	var valA = getRegister(this.rA),
+		valB = getRegister(this.rB),
 		valE = valB + this.D;
 	ST(valE, valA, 4);
 };
 INSTR[5] = function () {
-	var valB = REG[this.rB],
+	var valB = getRegister(this.rB),
 		valE = valB + this.D;
 	REG[this.rA] = LD(valE);
 };
 INSTR[6] = function () {
-	var valA = REG[this.rA],
-		valB = REG[this.rB],
+	var valA = getRegister(this.rA),
+		valB = getRegister(this.rB),
 		sgnA, sgnB, sgnR, signBit = 0x80000000;
 	switch(this.fn) {
 		case 0:
 			sgnA = !!(valA & signBit);
 			sgnB = !!(valB & signBit);
-			REG[this.rB] += REG[this.rA];
-			sgnR = !!(REG[this.rB] & signBit);
+			REG[this.rB] += getRegister(this.rA);
+			sgnR = !!(getRegister(this.rB) & signBit);
 			OF = +(sgnA && sgnB && !sgnR ||
 			       !sgnA && !sgnB && sgnR)
 			break;
 		case 1:
 			sgnA = !!(valA & signBit);
 			sgnB = !!(valB & signBit);
-			REG[this.rB] -= REG[this.rA];
-			sgnR = !!(REG[this.rB] & signBit);
+			REG[this.rB] -= getRegister(this.rA);
+			sgnR = !!(getRegister(this.rB) & signBit);
 			OF = +(sgnA && sgnB && !sgnR ||
 			       !sgnA && !sgnB && sgnR)
 			break;
 		case 2:
-			REG[this.rB] = REG[this.rA] & REG[this.rB];
+			REG[this.rB] = getRegister(this.rA) & getRegister(this.rB);
 			break;
 		case 3:
-			REG[this.rB] = REG[this.rA] ^ REG[this.rB];
+			REG[this.rB] = getRegister(this.rA) ^ getRegister(this.rB);
 			break;
 	}
-	SF = REG[this.rB] & 0x80000000 ? 1 : 0;
-	ZF = REG[this.rB] === 0 ? 1 : 0;
+	SF = getRegister(this.rB) & 0x80000000 ? 1 : 0;
+	ZF = getRegister(this.rB) === 0 ? 1 : 0;
 };
 INSTR[7] = function ()  {
 	switch(this.fn) {
@@ -141,30 +141,30 @@ INSTR[7] = function ()  {
 	}
 };
 INSTR[8] = function () {
-	var valB = REG[4],
+	var valB = getRegister(4),
 		valE = valB - 4;
 	ST(valE, PC, 4);
 	REG[4] = valE;
 	PC = this.Dest;
 };
 INSTR[9] = function () {
-	var valA = REG[4],
-		valB = REG[4],
+	var valA = getRegister(4),
+		valB = getRegister(4),
 		valE = valB + 4,
 		valM = LD(valA);
 		REG[4] = valE;
 		PC = valM;
 };
 INSTR[10] = function () {
-	var valA = REG[this.rA],
-		valB = REG[4],
+	var valA = getRegister(this.rA),
+		valB = getRegister(4),
 		valE = valB - 4;
 	ST(valE, valA, 4);
 	REG[4] = valE;
 };
 INSTR[11] = function () {
-	var valA = REG[4],
-		valB = REG[4],
+	var valA = getRegister(4),
+		valB = getRegister(4),
 		valE = valB + 4,
 		valM = LD(valA);
 	REG[4] = valE;

css/index.css
@@ -235,7 +235,7 @@ html, body {
 	font-family: 'Inconsolata';
 	font-size: 14px;
 
-	max-width: 420px;
+	max-width: 450px;
 	margin: 0 auto;
 }
 
@@ -277,6 +277,7 @@ html, body {
 
 .registers .flags,
 .registers .status,
+.registers .err,
 .registers .pc {
 	display: -webkit-flex;
 	display: flex;
@@ -294,6 +295,7 @@ html, body {
 }
 
 .registers .status .label,
+.registers .err .label,
 .registers .pc .label {
 	width: 34px;
 	margin-right: 6px;

index.html
@@ -157,6 +157,10 @@ Stack:
                 <div class="label">STAT</div>
                 <div class="value"><%= stat %></div>
             </div>
+            <div class="err">
+                <div class="label">ERR</div>
+                <div class="value"><%= err %></div>
+            </div>
             <div class="pc">
                 <div class="label">PC</div>
                 <div class="value"><%= pc %></div>

js/views/registers.js
@@ -29,6 +29,7 @@ var RegistersView = Backbone.View.extend({
 			zf: ZF,
 			of: OF,
 			stat: STAT,
+			err: ERR,
 			pc: '0x' + padHex(PC.toString(16), 4)
 		};
 

js/y86.js
@@ -7,7 +7,16 @@ var PC 		= 0,
 	STAT	= 'AOK',
 	MEMORY 	= new Uint8Array(MEM_SIZE),
 	SF = 0, ZF = 0, OF = 0,
-	ERR = 'AOK';
+	ERR = '';
+
+// Bounds check the register array
+function getRegister (idx) {
+	if (idx < 0 || idx > 8) {
+		STAT = 'INS';
+		throw new Error('Invalid register ID: 0x' + idx.toString(16));
+	}
+	return REG[idx];
+}
 
 // Uint32Arrays are not actually arrays
 Uint8Array.prototype.slice = function () {
@@ -25,16 +34,15 @@ function RESET() {
 	REG	= new Uint32Array(8);
 	STAT = 'AOK';
 	SF = 0; ZF = 0; OF = 0;
-	ERR = 'AOK';
+	ERR = '';
 }
 
 // Load
 function LD (addr) {
 	var result;
-	if (addr > MEMORY.length || addr < 0) {
+	if (addr < 0 || addr + 4 > MEM_SIZE) {
 		STAT = 'ADR';
-		print("Invalid address. PC = " + addr);
-		return 0;
+		throw new Error("Invalid address 0x" + addr.toString(16));
 	}
 	result  = MEMORY[addr];
 	result |= MEMORY[addr + 1] << 8;
@@ -46,12 +54,12 @@ function LD (addr) {
 // Store
 function ST(addr, data, bytes){
 	var result, i;
-	if (addr < 0) {
+	if (addr < 0 || addr + bytes > MEM_SIZE) {
 		STAT = 'ADR';
+		throw new Error("Invalid address 0x" + addr.toString(16));
 	}
 	if (typeof bytes === 'undefined') {
 		bytes = Math.ceil(Math.log(data + 1) / Math.log(16) / 2);
-		print('No Bytes, using ' + bytes)
 	}
 	for (i = 0; i < bytes; i++){
 		MEMORY[addr + i] = data & 0xFF;
@@ -296,6 +304,7 @@ function ASSEMBLE (raw) {
 function INIT (obj) {
 	MEMORY = toByteArray(obj);
 	STAT = 'AOK';
+	ERR = '';
 	RESET();
 }
 
@@ -353,7 +362,11 @@ function STEP () {
 	var args = DECODE(instr);
 
 	// Execute + Memory + Write Back ???
-	INSTR[icode].call(args);
+	try {
+		INSTR[icode].call(args);
+	} catch (e) {
+		ERR = e.message;
+	}
 }
 
 function hex2arr (str) {

commit 3829c9d154df66bf745b47c02e3edf032c35dce0

Fix cmovge
cmovge gives error message "Invalid left-hand side in assignment", which
was introduced by commit 09c768541adaaa336882b87a4ca0da1857f2a73b

js/instr.js
@@ -40,7 +40,7 @@ INSTR[2] = function () {
 		case 5:
 			// CMOVGE
 			if (SF === 0 || ZF === 1) {
-				getRegister(this.rB) = getRegister(this.rA);
+				REG[this.rB] = getRegister(this.rA);
 			}
 			break;
 		case 6: